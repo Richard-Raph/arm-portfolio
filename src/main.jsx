bdfd340274036161cbae48333be871d61a2efe65
Richard-Raph <richardakpan77@gmail.com>
2024-05-29 13:29:51 +0100

    "Updated index.html with new meta tags, added @fontsource/roboto and @mui/icons-material dependencies, removed src/index.css, and updated src/main.jsx to import Roboto fonts."

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
