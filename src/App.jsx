0f3fd2b711b9ce3604b62c72c8260ff86bcd0d56
Richard-Raph <richardakpan77@gmail.com>
2024-07-30 02:37:10 +0100

    "Remove DotRing component and its CSS file"

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import Top from './layouts/Top';
 import Home from './routes/Home';
 import About from './routes/About';
 import Project from './routes/Project';
-// import DotRing from './components/DotRing/DotRing';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Top />
-      {/* <DotRing /> */}
       <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
