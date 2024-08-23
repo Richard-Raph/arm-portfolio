d7a3028d71d67df39be6fe9f2aa3ac7825695c8c
Richard-Raph <richardakpan77@gmail.com>
2024-08-23 18:43:56 +0100

    "Refactor Hero and DockBar components: update CSS styles, rename classes, and restructure JSX elements."

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,46 +5,47 @@ import { HiPlus, HiOutlineArrowDown } from 'react-icons/hi2';
 const Hero = () => {
     return (
         <section className='hero'>
-                <div className='hero-fade'>
-                    <Link><HiOutlineArrowDown /></Link>
-                </div>
-                <div className='container-hero-image'>
-                    <div className='glare-item-top outer-edge'></div>
-                    <div className='window-outline'>
-                        <div className='glare-item-top hero-inner'></div>
-                        <div className='window-main'>
-                            <div className='shine-wrapper'>
-                                <div className='shine-small'></div>
-                                <div className='shine-big'></div>
+            <div className='hero-container'>
+                <span className='glare outer'></span>
+                <div className='window-outline'>
+                    <span className='glare inner'></span>
+                    <div className='window-main'>
+                        <div className='shine'>
+                            <div className='small'></div>
+                            <div className='big'></div>
+                        </div>
+                        <div className='window-bar'>
+                            <div className='window-dots'>
+                                <span className='dot red'></span>
+                                <span className='dot yellow'></span>
+                                <span className='dot green'></span>
                             </div>
-                            <div className='window-bar'>
-                                <div className='window-dots'>
-                                    <span className='dot red'></span>
-                                    <span className='dot yellow'></span>
-                                    <span className='dot green'></span>
+                            <HiPlus />
+                        </div>
+                        <div className='window-content'>
+                            <div className='hero-content'>
+                                <div className='text-hero'>
+                                    I craft products, interactions &amp; 
+                                    <span className='text-hero-serif'>stories.</span>
                                 </div>
-                                <HiPlus />
-                            </div>
-                            <div className='window-content'>
-                                <div className='hero-text-content'>
-                                    <div className='text-hero'>
-                                        I craft products, interactions &amp;<span className='text-hero-serif'>stories.</span>
-                                    </div>
-                                    <div className='hero-about-grid'>
-                                        <div className='hero-about-null'></div>
-                                        <div>
-                                            <div className='text-hero-about'>
-                                                Instructor at <Link to='htps://myteacher.ng'>Myteacher</Link>. Based in Port Harcourt.<br/>
-                                                <span className='text-hero-about-formerly'>Formerly at Google and RBC.</span>
-                                            </div>
+                                <div className='hero-about-grid'>
+                                    <span className='hero-about-null'></span>
+                                    <div>
+                                        <div className='text-hero-about'>
+                                            Instructor at <Link to='htps://myteacher.ng'>Myteacher</Link>. Based in Port Harcourt.<br />
+                                            <span className='text-hero-about-formerly'>Formerly at Google and RBC.</span>
                                         </div>
                                     </div>
                                 </div>
-                                <div className='window-grain'></div>
                             </div>
+                            <span className='window-grain'></span>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className='hero-fade'>
+                <Link><HiOutlineArrowDown /></Link>
+            </div>
         </section>
     );
 }
