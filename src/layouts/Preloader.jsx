70e5c4fa353a3f5418086a046bb321ad34134581
Richard-Raph <richardakpan77@gmail.com>
2024-08-30 18:11:19 +0100

    "Added Lordicon script to index.html, updated App.css and HHero.css styles, and replaced Preloader component with Lordicon animation"

--- src/layouts/Preloader.jsx
+++ src/layouts/Preloader.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
+
 export default function Preloader() {
-    return (
-        <div className='back'>
-            <div className='cont'>
-                <div className='spinner'></div>
-                <span className='number'></span>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    if (window.lord) {
+      window.lord.init();
+    }
+  }, []);
+
+  return (
+    <div className='preloader'>
+      <lord-icon
+        delay='1500'
+        trigger='loop'
+        state='in-reveal'
+        style={{ width: '100px', height: '100px' }}
+        src='https://cdn.lordicon.com/gyevrheg.json'
+        colors='primary:#000000,secondary:#794628,tertiary:#ffffff,quaternary:#000000,quinary:#000000'
+      ></lord-icon>
+    </div>
+  );
 }
