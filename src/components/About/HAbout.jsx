9e4f623a1249c20ce806d3432b2cd8f7bca50fc6
Richard-Raph <richardakpan77@gmail.com>
2024-09-12 02:04:16 +0100

    "Updated HAbout.css and HAbout.jsx files: added flex-direction to .habout class, uncommented .paper div, and removed commented out code blocks."

--- src/components/About/HAbout.jsx
+++ src/components/About/HAbout.jsx
@@ -7,14 +7,14 @@ const HAbout = () => {
     return (
         <section className='habout' data-aos='fade-up' id='about'>
             <h1 className='slant-text'>About Me</h1>
-            {/* <div className='paper'>
+            <div className='paper'>
                 <div className='pin'>
                     <span className='metal'></span>
                     <span className='shadow'></span>
                     <span className='bottom-circle'></span>
                 </div>
                 <h2>About Me</h2>
-            </div> */}
+            </div>
             <div className='habout-content'>
                 <div className='habout-img'>
                     <img src={Doodle} alt='doodle' />
@@ -37,15 +37,6 @@ const HAbout = () => {
 }
 
 export default HAbout;
-{/* <div className='paper'>
-  <div className='lines'>
-    <div className='text' contentEditable spellCheck='false'></div>
-  </div>
-  <div className='holes hole-top'></div>
-  <div className='holes hole-middle'></div>
-  <div className='holes hole-bottom'></div>
-</div> */}
-
 {/* <div className='notepad'>
   <div className='top'></div>
   <div className='paper' contentEditable='true'></div>
