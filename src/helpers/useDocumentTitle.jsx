09ee35e755b53c48954df03738156ea746bcc0fb
Richard-Raph <richardakpan77@gmail.com>
2024-10-29 16:49:06 +0100

    simplified useEffect

--- src/helpers/useDocumentTitle.jsx
+++ src/helpers/useDocumentTitle.jsx
@@ -4,7 +4,5 @@ import { useContext, useEffect } from 'react';
 export default function useDocumentTitle(newTitle) {
     const { setTitle } = useContext(TitleContext);
 
-    useEffect(() => {
-        setTitle(newTitle);
-    }, [newTitle, setTitle]);
+    useEffect(() => setTitle(newTitle), [newTitle, setTitle]);
 }
